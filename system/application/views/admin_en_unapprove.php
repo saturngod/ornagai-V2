98abdab5545182a1cf81219b1af02b13b5994707
Saturngod <saturngod@gmail.com>
2010-02-27 20:38:42 +0800

    fix admin unapprove design & starting en_unapprove_word

--- system/application/views/admin_en_unapprove.php
+++ system/application/views/admin_en_unapprove.php
@@ -4,31 +4,49 @@ $data['base']=$base;
 $this->load->view("adminheader_view",$data);
 ?>
 <div id="unapprove">
-<ul class="approve_list">
+
+<a href="#" id="approve">Approve</a>
+<table border="0" cellpadding="0" cellspacing="0" class="table_admin">
+<tr class="table_header">
+<td>
+<input type=checkbox id="chk_all" value='' />
+</td>
+<td>
+Word
+</td>
+<td>
+State
+</td>
+<td>
+Defination
+</td>
+<td>
+Username
+</td>
+</tr>
 <?php
 foreach ($query->result() as $row)
 {
-	
-	echo "<li>";
-	echo "<ul>";
-	echo "<li>";
-	echo "<input type=checkbox name='enword[]' value=".$row->word_id." />";
-	echo "</li>";
-	echo "<li>";
+	echo "<tr>";
+	echo "<td>";
+	echo "<input class='enword' type=checkbox value=".$row->word_id." />";
+	echo "</td>";
+	echo "<td>";
 	echo $row->Word;
-	echo "</li>";
-	echo "<li>";
+	echo "</td>";
+	echo "<td>";
 	echo $row->state;
-	echo "</li>";
-	echo "<li>";
+	echo "</td>";
+	echo "<td>";
 	echo $row->def;
-	echo "</li>";
-	echo "<li>";
+	echo "</td>";
+	echo "<td>";
 	echo $row->username;
-	echo "</li>";
+	echo "</td>";
+	echo "</tr>";
 }
 ?>
-</ul>
+</table>
 </div>
 <?php
 $data['title']=$title;
