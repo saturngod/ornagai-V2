fcd2e956edb746e05c00ce2946b9ea6b30842d82
Saturngod <saturngod@gmail.com>
2010-03-14 20:32:34 +0800

    fix jqtouch css & embed font speed

--- system/application/views/touch_result.php
+++ system/application/views/touch_result.php
@@ -6,26 +6,15 @@
     <ul class="edgetoedge">
     	<?php
     	$output="";
-    	$i=0;
     	foreach ($query  as $row)
     	{
-    	$i=$i+1;
     	?>
     	<li class="sep"><?= $row->Word ?>( <?= $row->state ?> )</li>
-    	<li><a class="zg" href="#more<?= $i ?>"><?= str_replace("|","",$row->def) ?></a></li>
+    	<li><a class="zg" href="#"><?= str_replace("|","",$row->def) ?></a></li>
     	<?php
-    	$output.='<div id="more'.$i.'"><script type="text/javascript">
-    				//load Zawgyi
-    				Cufon.DOM.ready(function() {
-    	    			Cufon.replace(".zg");
-    	    		});
-    	</script><div class="zg" style="font-size: 1.5em; text-align: center; margin: 160px 0 160px; font-family: Marker felt;">'.str_replace("|","",$row->def).'
-    	    </div>
-    	    <a style="margin:0 10px;color:rgba(0,0,0,.9)" href="#" class="whiteButton goback">Back</a>
-    	</div>';
     	}
     	?>
-    </ul>
+    	</ul>
     <style>
     .zg
     {
@@ -39,6 +28,3 @@
         		});
     </script>
 </div>
-<?php
-echo $output;
-?>
