5ab866abcabde67d1bbf7e4cdba7664a8f928fc0
Saturngod <saturngod@gmail.com>
2010-03-21 13:38:10 +0800

    start admin with more feature

--- system/application/views/admin_dashboard_view.php
+++ system/application/views/admin_dashboard_view.php
@@ -9,8 +9,10 @@ $this->load->view("adminheader_view",$data);
             <li><a href="<?= $base ?>/index.php/admin/users">Users</a></li>
             <li><a href="<?= $base ?>/index.php/admin/enunapprove">Unapprove English Word</a></li>
             <li><a href="<?= $base ?>/index.php/admin/myunapprove">Unapprove Myanmar Word</a></li>
-            <li>Approve</li>
-            <li>Total</li>
+            <li>EN Approve</li>
+            <li>EN Total</li>
+            <li>EN Approve</li>
+            <li>EN Total</li>
         </ul>
         </div>
         
@@ -19,8 +21,10 @@ $this->load->view("adminheader_view",$data);
             <li><?= $total_users ?></li>
             <li><?= $en_unapprove ?></li>
             <li><?= $my_unapprove ?></li>
-            <li>Approve</li>
-            <li>Total</li>
+            <li>EN Approve</li>
+            <li>EN Total</li>
+            <li>MM Approve</li>
+            <li>MM Total</li>
         </ul>
         </div>
     </div>
