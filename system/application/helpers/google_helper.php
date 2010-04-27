7f7251ee87da5b79b4a819ec95bb4244ed0f0217
Saturngod <saturngod@gmail.com>
2010-04-27 14:23:32 +0800

    change analytics code

--- system/application/helpers/google_helper.php
+++ system/application/helpers/google_helper.php
@@ -54,13 +54,17 @@ function google_analytics($uacct = '')
         if($CI->config->slash_item('google_uacct') != ''){
             
             // Found config setting
+            
             $google_analytics_code = '
-                <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-                </script>
-                <script type="text/javascript">
-                _uacct = "'.$CI->config->item('google_uacct').'";
-                urchinTracker();
-                </script>
+               <script type="text/javascript">
+               var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+               document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+               </script>
+               <script type="text/javascript">
+               try {
+               var pageTracker = _gat._getTracker("'.$CI->config->item('google_uacct').'");
+               pageTracker._trackPageview();
+               } catch(err) {}</script>
             ';
             
         }
@@ -71,14 +75,18 @@ function google_analytics($uacct = '')
         
         if($uacct != ''){
             
+            
             // Found $uacct variable data
             $google_analytics_code = '
-                <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-                </script>
-                <script type="text/javascript">
-                _uacct = "'.$uacct.'";
-                urchinTracker();
-                </script>
+               <script type="text/javascript">
+               var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+               document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+               </script>
+               <script type="text/javascript">
+               try {
+               var pageTracker = _gat._getTracker("'.$uacct.'");
+               pageTracker._trackPageview();
+               } catch(err) {}</script>
             ';
             
         }
