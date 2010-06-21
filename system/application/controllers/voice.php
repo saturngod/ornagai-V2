d23a9dc9a3f21bde1e0a9377692e5b2cc79c7d2d
Saturngod <saturngod@gmail.com>
2010-06-22 00:57:33 +0800

    add audio for safari with HTML5 audio

--- system/application/controllers/voice.php
+++ system/application/controllers/voice.php
@@ -14,7 +14,20 @@ class Voice extends Controller {
 		$data['base']=$this->config->item('base_url');
 		$data['sound']="http://translate.google.com/translate_tts?tl=en&q=".stripslashes($this->uri->segment(3));
 		$data['sound']=urlencode($data['sound']);
+		$data['mp3']=false;
+		$this->load->library('user_agent');
+		
+		if ($this->agent->is_browser())
+		{
+		    if($this->agent->browser()=="Safari")
+		    {
+		    	$data['mp3']=true;
+		    }
+		}
+		
 		$this->load->view('voice.php',$data);
+		
+		
 	}
 }
 ?>
