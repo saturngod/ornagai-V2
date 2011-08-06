83f9b87d86727c21c8c6682c59305075ff0a71e4
saturngod <saturngod@gmail.com>
2011-08-06 21:11:12 +0800

    clean files

--- system/application/views/admin_user_edit.php
+++
@@ -1,9 +0,0 @@
-<input type="hidden" id="userid" value="<?php echo $userinfo->id?>" />
-<label>Username</label>
-<input type="text" id="username" value="<?php echo $userinfo->username ?>">
-<label>Email</label>
-<input type="email" id="email" value="<?php echo $userinfo->email ?>" />
-<label>JoinDate</label>
-<input type="text" id="date" value="<?php echo $userinfo->join_date ?>" />
-<label>Ban</label>
-<input type="checkbox" id="ban" <?php echo ($userinfo->ban==1) ? "checked":"" ?> />
--- system/application/views/admin_word_edit.php
+++
@@ -1,6 +0,0 @@
-<label>Word</label>
-<input type="word" name="" value="<?php echo $wordinfo->Word ?>" />
-<label>State</label>
-<input type="state" name="" value="<?php echo $wordinfo->state ?>" />
-<label>Def</label>
-<input type="def" name="" value="<?php echo $wordinfo->def ?>" />
