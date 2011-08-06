9ec9285938fa9285f4dfc1e6ae06de540fa62735
saturngod <saturngod@gmail.com>
2011-08-06 19:00:51 +0800

    fix recaptcha

---
+++ application/config/recaptcha.php
@@ -0,0 +1,15 @@
+<?
+/*
+The reCaptcha server keys and API locations
+
+Obtain your own keys from:
+http://www.recaptcha.net
+*/
+$config['recaptcha'] = array(
+  'public'=>'6Leg0QgAAAAAAG56ibAYPZ8K8BZhElJ5OyzwmkIL ',
+  'private'=>'6Leg0QgAAAAAAFTBTIwwQ9Rdp0Tdl0XmhUPU4RHg',
+  'RECAPTCHA_API_SERVER' =>'http://api.recaptcha.net',
+  'RECAPTCHA_API_SECURE_SERVER'=>'https://api-secure.recaptcha.net',
+  'RECAPTCHA_VERIFY_SERVER' =>'api-verify.recaptcha.net',
+  'theme' => 'white'
+);
