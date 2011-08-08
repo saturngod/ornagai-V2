2f414c1ad3ad840868f37d3c62f0501a680756aa
Htain Lin Shwe <saturngod@gmail.com>
2011-08-08 19:53:16 +0800

    remove reinvigorate

--- application/views/footer_view.php
+++ application/views/footer_view.php
@@ -2,13 +2,5 @@
 $this->load->helper("google");
 echo google_analytics();
 ?>
-<!--
-<script type="text/javascript" src="http://include.reinvigorate.net/re_.js"></script>
-<script type="text/javascript">
-try {
-reinvigorate.track("71f36-anodd21w3e");
-} catch(err) {}
-</script>
--->
 </body>
 </html>
